806485f140c05b9fc18cf322f7da50f8207f4158
Mateo Bernasconi Vargas <bernasconivargas.mateo@gmail.com>
2023-07-17 20:19:43 -0400

    created react project

---
+++ reactChat/src/App.tsx
@@ -0,0 +1,21 @@
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
